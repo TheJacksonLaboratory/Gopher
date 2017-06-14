651df028b45e8e391b0e0f8f1b7a182fa5480338
phansen <peter.hansen@charite.de>
2017-06-14 18:33:02 +0200

    Worked on ViewPointTest class.

--- src/test/java/vpvgui/model/project/ViewPointTest.java
+++ src/test/java/vpvgui/model/project/ViewPointTest.java
@@ -4,6 +4,8 @@ import htsjdk.samtools.reference.IndexedFastaSequenceFile;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -14,13 +16,33 @@ public class ViewPointTest {
     public void testConstructor() throws Exception {
 
         /* Create string array for cutting patterns */
-        String[] Patterns = new String[]{"ACGT","CTGA"};
+        String[] cuttingPatterns = new String[]{"TG","CA"};
 
         /* Create indexed sequence file */
         final File fasta = new File("src/test/resources/smallgenome/chr4_ctg9_hap1.fa"); // index has to be there
         IndexedFastaSequenceFile fastaReader = new IndexedFastaSequenceFile(fasta);
 
         /* Call Contructor */
-        ViewPoint myViewpoint = new ViewPoint("chr4_ctg9_hap1",100, Patterns, fastaReader);
+        ViewPoint myViewpoint = new ViewPoint("chr4_ctg9_hap1",10000, cuttingPatterns, fastaReader);
+
+        // print out whole hash map to show how to access the elements
+        System.out.println();
+        System.out.println(fastaReader.getSubsequenceAt("chr4_ctg9_hap1",10000-100,10000+100).getBaseString());
+        String tssRegionString = fastaReader.getSubsequenceAt("chr4_ctg9_hap1",10000-100,10000+100).getBaseString(); // get sequence around TSS
+        for(int i=0;i<cuttingPatterns.length;i++) {
+            System.out.println();
+            ArrayList<Integer> arr1 = myViewpoint.getCuttingPositionMap().get(cuttingPatterns[i]);
+            for(int j=0;j<arr1.size();j++) {
+                String s = new String("");
+                for(int k=0;k<100+arr1.get(j);k++) {
+                    s += " ";
+                }
+                s += fastaReader.getSubsequenceAt("chr4_ctg9_hap1",10000+arr1.get(j),10000+arr1.get(j)+cuttingPatterns[i].length()-1).getBaseString();
+                s += " ";
+                s += arr1.get(j);
+                System.out.println(s);
+            }
+            //System.out.print('\n');
+        }
         }
 }
