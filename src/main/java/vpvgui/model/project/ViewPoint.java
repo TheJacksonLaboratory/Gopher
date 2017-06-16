b9ca1944eaa5a3ef130821d87de05c24124453b1
phansen <peter.hansen@charite.de>
2017-06-16 13:51:08 +0200

    Worked on ViewPoint class.

--- src/main/java/vpvgui/model/project/ViewPoint.java
+++ src/main/java/vpvgui/model/project/ViewPoint.java
@@ -12,12 +12,12 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 /*
- * A region, usually at the transcription start site of a gene,
- * that will be enriched in a capture C experiment.
- * The start and end coordinates can be set manually or automatically.
- * In the latter case one of two approaches can be used.
- * Start and end coordinates can be altered after automatic generation.
- * The last approach of derivation will be tracked.
+ A region, usually at the transcription start site of a gene,
+ that will be enriched in a capture C experiment.
+ The start and end coordinates can be set manually or automatically.
+ In the latter case one of two approaches can be used.
+ Start and end coordinates can be altered after automatic generation.
+ The last approach of derivation will be tracked.
  */
 
 public class ViewPoint {
@@ -28,6 +28,10 @@ public class ViewPoint {
     /* Coordinate of the original TSS */
     private Integer tssPos;
 
+    /* The viewpoint cannot be outside the interval [maxUpstreamTssPos,maxDownPos]. */
+    private Integer maxUpstreamTssPos;
+    private Integer maxDownstreamPos;
+
     /* Start and end position of the viewpoint */
     private Integer startPos, endPos;
 
@@ -35,7 +39,7 @@ public class ViewPoint {
     private String geneSymbol;  // IN FACT, A VIEWPOINT SHOULD POINT TO A GENE OBJECT!
 
     /* Derivation approach, either combined (CA), Andrey et al. 2016 (AEA), or manually (M) */
-    private StringProperty derivationApproach;
+    private String derivationApproach;
 
     /* A hash map with restriction cutting sites as key and arrays of integers as values.
      * The integer values are positions relative to the TSS. */
@@ -43,13 +47,20 @@ public class ViewPoint {
 
 
     /* Constructor fuction */
-    public ViewPoint(String referenceSequenceID, Integer tssPos, Integer initialRadius, String[] cuttingPatterns, IndexedFastaSequenceFile fastaReader) {
 
-        /* Set referenceSequenceID */
+    public ViewPoint(String referenceSequenceID, Integer tssPos, Integer initialRadius, String[] cuttingPatterns, IndexedFastaSequenceFile fastaReader, String geneSymbol) {
+
+        /* Set fields */
+
         setReferenceID(referenceSequenceID);
-        System.out.println(getReferenceID());
+        setTssPos(tssPos);
+        setStartPos(tssPos - initialRadius);
+        setEndPos(tssPos + initialRadius);
+        setGeneSymbol(geneSymbol);
+        setDerivationApproach("INITIAL");
 
-        /* Init hash of int arrays */
+        /* Create hash of int arrays */
+
         cuttingPositionMap=createCuttingPositionMap(referenceSequenceID, tssPos, initialRadius, cuttingPatterns, fastaReader);
     }
 
@@ -74,6 +85,20 @@ public class ViewPoint {
     }
 
 
+    public final void setMaxUpstreamTssPos(Integer maxUpstreamTssPos) { this.maxUpstreamTssPos=maxUpstreamTssPos; }
+
+    public final Integer getMaxUpstreamTssPos() { return maxUpstreamTssPos; }
+
+
+    public final void setMaxDownstreamPos(Integer maxDownstreamPos) {
+        this.maxDownstreamPos=maxDownstreamPos;
+    }
+
+    public final Integer getMaxDownstreamPos() {
+        return maxDownstreamPos;
+    }
+
+
     public final void setStartPos(Integer startPos) {
         this.startPos=startPos;
     }
@@ -90,6 +115,21 @@ public class ViewPoint {
     public final void setEndPos(Integer endPos) {
         this.endPos=endPos;
     }
+
+
+    public final String getGeneSymbol() { return geneSymbol;    }
+
+    public final void setGeneSymbol(String geneSymbol) { this.geneSymbol=geneSymbol;    }
+
+
+    public final String getDerivationApproach() { return derivationApproach;    }
+
+    public final void setDerivationApproach(String derivationApproach) { this.derivationApproach=derivationApproach;    }
+
+    public Integer getGenomicPosFromTssRelativePos(Integer tssPos,Integer tssRelPos) {
+        return this.tssPos+tssRelPos;
+    }
+
 
     //----------------------------------------
 
@@ -109,14 +149,13 @@ public class ViewPoint {
 
         for(int i=0;i<cuttingPatterns.length;i++) {
 
-            String tssRegionString = fastaReader.getSubsequenceAt(referenceSequenceID,tssPos-initialRadius,tssPos+initialRadius).getBaseString(); // get sequence around TSS
+            String tssRegionString = fastaReader.getSubsequenceAt(referenceSequenceID,tssPos-initialRadius,tssPos+initialRadius).getBaseString().toUpperCase(); // get sequence around TSS
+
             Pattern pattern = Pattern.compile(cuttingPatterns[i]);
             Matcher matcher = pattern.matcher(tssRegionString);
-            ArrayList<Integer> cuttingPositionList = new ArrayList<Integer>(); //int[] cuttingPositions;
+            ArrayList<Integer> cuttingPositionList = new ArrayList<Integer>();
 
-            int count = 0;
             while(matcher.find()) {
-                count++;
                 cuttingPositionList.add(matcher.start()-initialRadius); // push occurence positions relative to the TSS to list.
             }
 
@@ -125,6 +164,30 @@ public class ViewPoint {
 
         return cuttingPositionMap;
     }
+
+    private void extendFragmentWise(Integer fromThisPos){
+
+        /* The viewpoint will be extended up to the next cutting site. */
+
+            /* CASE 1: If 'fromThisPos' is upstream of the interval ['tssPos'-'initialRadius','tssPos'+'initialRadius'],
+                    the viewpoint extended to the outmost cutting site upstream within this interval. */
+
+            /* CASE 2: If 'fromThisPos' is upstream of 'startPos' and within the ['tssPos'-'initialRadius','tssPos'+'initialRadius'],
+                    'startPos' will be set to position of the next cutting site upstream of 'fromThisPos'. */
+
+            /* CASE 3: If 'fromThisPos' within the interval [startPos,endPos],
+                    nothing will happen. */
+
+            /* CASE 4: If 'fromThisPos' is downstream of 'endPos' and within the [tssPos-initialRadius,tssPos+initialRadius],
+                    'endPos' will be set to position of the next cutting site downstream of 'fromThisPos'. */
+
+            /* CASE 5: If 'fromThisPos' is downstream of the interval ['tssPos'-'initialRadius','tssPos'+'initialRadius'],
+                    the viewpoint extended in to the outmost cutting site downstream within this interval. */
+
+
+    }
+
+
 
 
 }
