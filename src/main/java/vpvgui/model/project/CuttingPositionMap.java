5636c2252d07c68ad2ded12d9ec89ea6c663c5e7
phansen <peter.hansen@charite.de>
2017-06-22 14:47:43 +0200

    Added javadoc documentation.

--- src/main/java/vpvgui/model/project/CuttingPositionMap.java
+++ src/main/java/vpvgui/model/project/CuttingPositionMap.java
@@ -32,14 +32,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * This a utility class for the class 'ViewPoint'.
+ * This is a utility class for the class 'ViewPoint' that holds the cutting positions for the provided restriction enzyme motifs.
+ * The cutting positions are calculated when an object of the class is created.
+ * <p>
+ * The most central data structure in this class is a <i>HashMap</i> with 'Strings' as keys and <i>ArrayLists</i> of <i>Integers</i> as values.
+ * The 'Strings' are cutting motifs and the related 'ArrayList' contains all positions of occurrences within a given range.
+ * <p>
+ * In addition to the usual getter and setter function, the class provides a functions that allow to query the 'HashMap'.
  * @author Peter Hansen
  */
 public class CuttingPositionMap {
 
     /* fields */
 
-    String referenceSequenceID;
     Integer genomicPos;
     Integer maxDistToTssUp;
     Integer maxDistToTssDown;
@@ -49,12 +54,25 @@ public class CuttingPositionMap {
 
     /* constructor */
 
-    public CuttingPositionMap(IndexedFastaSequenceFile fastaReader, String referenceSequenceID, Integer genomicPos, String[] cuttingPatterns, Integer maxDistToTssUp, Integer maxDistToTssDown) {
-
+    /**
+     * The constructor will set all fields of this class and create the actual <i>HashMap</i> for cutting positions,
+     * which will be derived only for the interval <i>[genomicPos-maxDistToTssUp,genomicPos+maxDistToTssDown]</i>.
+     * <p>
+     * The keys for the <i>HashMap</i> will be the same as passed by the argument <i>cuttingPatterns</i>,
+     * but with 'characters' removed.
+     * The information about the cutting position within in a particular motif will be recorded in  <i>cuttingPositionMapOffsets</i>, an additional  <i>HashMap</i>.
+     * In addition to the passed motifs, there is one special key <i>ALL</i> which contains the cutting positions for the union of all motifs.
+     * @param referenceSequenceID name of the genomic sequence, e.g. <i>chr1</i>.
+     * @param genomicPos central position of the region for which the CuttingPositionMap is created.
+     * @param maxDistToTssUp maximal distance to 'genomicPos' in upstream direction.
+     * @param maxDistToTssDown maximal distance to 'genomicPos' in downstream direction.
+     * @param fastaReader indexed FASTA file that contains the sequence information required for the calculation of cutting positions.
+     * @param cuttingPatterns array of cutting motifs, e.g. <i>A^AGCTT</i> for the resrtiction enzyme <i>HindIII</i>. The '^' indicates the cutting position within the motif.
+     */
+    public CuttingPositionMap(String referenceSequenceID, Integer genomicPos, IndexedFastaSequenceFile fastaReader, Integer maxDistToTssUp, Integer maxDistToTssDown, String[] cuttingPatterns) {
 
         /* set fields */
 
-        setReferenceID(referenceSequenceID);
         setGenomicPos(genomicPos);
         setMaxDistToTssUp(maxDistToTssUp);
         setMaxDistToTssDown(maxDistToTssDown);
@@ -71,8 +89,6 @@ public class CuttingPositionMap {
             cuttingPatterns[i] = cuttingPatterns[i].replace("^",""); // remove '^' characters
             cuttingPositionMapOffsets.put(cuttingPatterns[i],offSet);
         }
-
-
 
         ArrayList<Integer> cuttingPositionListUnion = new ArrayList<Integer>();
         for(int i=0;i<cuttingPatterns.length;i++) {
@@ -112,15 +128,6 @@ public class CuttingPositionMap {
 
     /* getter and setter functions */
 
-    public final String getReferenceID() {
-        return referenceSequenceID;
-    }
-
-    public final void setReferenceID(String setReferenceID) {
-        this.referenceSequenceID=setReferenceID;
-    }
-
-
     public final void setGenomicPos(Integer genomicPos) { this.genomicPos=genomicPos; }
 
     public final Integer getGenomicPos() {
@@ -150,6 +157,7 @@ public class CuttingPositionMap {
         return cuttingPositionMapOffsets;
     }
 
+
     /* utility functions */
 
     /**
@@ -160,6 +168,8 @@ public class CuttingPositionMap {
      * @param direction Direction in which the next cutting site will be searched.
      * @return Position of the next cutting position relative to 'genomicPos'.
      * @throws IllegalArgumentException if a value different than 'up' or 'down' is passed as 'direction' parameter.
+     * @throws IntegerOutOfRangeException if 'pos' is not within the interval [-maxDistToTssUp,maxDistToTssDown].
+     * @throws NoCuttingSiteFoundUpOrDownstreamException if there is no cutting position up or downstream of 'pos'. Exception is handled by returning the position of outermost cutting site up or downstream.
      */
     public Integer getNextCutPos(Integer pos, String direction) throws IllegalArgumentException, IntegerOutOfRangeException, NoCuttingSiteFoundUpOrDownstreamException {
 
