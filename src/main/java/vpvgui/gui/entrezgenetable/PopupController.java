8ae21e8b5540f5b8f30ace670b7d34a53dee31a0
pnrobinson <peter.robinson@jax.org>
2017-06-29 17:41:37 -0400

    additions to popup genes window

--- src/main/java/vpvgui/gui/entrezgenetable/PopupController.java
+++ src/main/java/vpvgui/gui/entrezgenetable/PopupController.java
@@ -4,6 +4,8 @@ package vpvgui.gui.entrezgenetable;
  * Created by peter on 03.06.17.
  */
 import javafx.event.ActionEvent;
+
+import java.io.*;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
@@ -11,6 +13,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import vpvgui.model.Model;
 import vpvgui.model.project.JannovarGeneGenerator;
@@ -19,6 +22,8 @@ public class PopupController  implements Initializable {
 
     @FXML private TextArea geneSymbolArea;
     @FXML private Button geneButton;
+    @FXML private Button uploadGenesButton;
+    @FXML private Button acceptGenesButton;
 
     @FXML private Label instructions;
 
@@ -103,5 +108,36 @@ public class PopupController  implements Initializable {
         }
         return fields;
     }
+
+
+    public void uploadGenes(ActionEvent e) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file == null) {
+            System.err.println("[ERROR] COuld not open genes file -TODO throw exception");
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        try {
+            BufferedReader br =new BufferedReader(new FileReader(file));
+            String line =null;
+            while ((line=br.readLine())!=null) {
+                System.out.println(line);
+                sb.append(line);
+            }
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+        String[] symbols = parseGeneSymbols(sb.toString());
+        if (this.model!=null)
+            this.model.setGeneSymbols(symbols);
+        e.consume();
+        geneSymbolArea.clear();
+        parseGeneSymbolsWithJannovar(symbols);
+
+    }
+
+
+
 
 }
