723347154f42cedb4b80f3a071112f43bac5b5b1
Daniel Danis <daniel.danis@savba.sk>
2017-07-26 16:53:45 -0400

    Adjust "select Entrez genes" dialog

--- src/main/java/vpvgui/gui/entrezgenetable/EntrezGenePresenter.java
+++ src/main/java/vpvgui/gui/entrezgenetable/EntrezGenePresenter.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
