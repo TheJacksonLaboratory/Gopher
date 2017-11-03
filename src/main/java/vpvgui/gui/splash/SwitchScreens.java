8169a271dd3bec64d30b2f3137e12cba0d29e0bf
pnrobinson <peter.robinson@charite.de>
2017-11-03 08:32:46 -0400

    Fixing null pointer bug in splash screen

--- src/main/java/vpvgui/gui/splash/SwitchScreens.java
+++ src/main/java/vpvgui/gui/splash/SwitchScreens.java
@@ -24,8 +24,6 @@ public class SwitchScreens {
     private static final Logger logger = Logger.getLogger(SwitchScreens.class.getName());
     /** A reference to the primary stage of the application. */
     private Stage primarystage = null;
-    /** A reference to the model of the application. */
-    private Model model=null;
     /** The width of the current screen, which is calculated in {@link vpvgui.gui.viewpointpanel.ViewPointView}. 800 is a default value,
      * but the variable will always be set to the current value of the user's screen.*/
     private int screenWidth=800;
@@ -41,20 +39,17 @@ public class SwitchScreens {
     public void createNewProject(String name){
         VPVMainView appView = new VPVMainView();
         VPVMainPresenter presenter = (VPVMainPresenter) appView.getPresenter();
-//        this.primarystage.setMinWidth(1000);
-//        this.primarystage.setWidth(1600);
         Model model = new Model();
         model.setProjectName(name);
         model.setDefaultValues();
         model.setXdim(screenWidth);
         model.setYdim(screenHeight);
-        logger.error(String.format("Setting dimensions to x=%d y=%d",screenWidth,screenHeight ));
+        logger.trace(String.format("Setting dimensions to x=%d y=%d",screenWidth,screenHeight ));
         presenter.setModel(model);
         Scene scene = new Scene(appView.getView());
         this.primarystage.setScene(scene);
         this.primarystage.centerOnScreen();
         this.primarystage.setTitle(String.format("Viewpoint Viewer: %s",name));
-        setModelXYBounds();
         this.primarystage.show();
     }
 
@@ -62,8 +57,9 @@ public class SwitchScreens {
         VPVMainView appView = new VPVMainView();
         VPVMainPresenter presenter = (VPVMainPresenter) appView.getPresenter();
         String filepath = Platform.getAbsoluteProjectPath(name);
+        Model model=null;
         try {
-            this.model = SerializationManager.deserializeModel(filepath);
+            model = SerializationManager.deserializeModel(filepath);
         } catch (IOException e) {
             ErrorWindow.displayException("IOException",String.format("I/O problem while attempting to deserialize %s",filepath),e);
             return;
@@ -74,15 +70,16 @@ public class SwitchScreens {
             ErrorWindow.displayException("Exception",String.format("Exception while attempting to deserialize %s",filepath),e);
             return;
         }
-        if (this.model == null) {
+        if (model == null) {
             logger.error(String.format("Unable to deserialize model from %s at %s", name, filepath));
             ErrorWindow.display("Null pointer", String.format("Unable to deserialize model from %s at %s", name, filepath));
             return;
         }
-        if (this.model.getHttpProxy() != null) {
+        if (model.getHttpProxy() != null) {
             Utils.setSystemProxyAndPort(model.getHttpProxy(),model.getHttpProxyPort());
         }
-
+        model.setXdim(screenWidth);
+        model.setYdim(screenHeight);
         presenter.setModelInMainAndInAnalysisPresenter(model);
         logger.trace("Deserialized model "+ model.getProjectName());
         if (presenter==null){
@@ -96,12 +93,16 @@ public class SwitchScreens {
         this.primarystage.setScene(scene);
         this.primarystage.centerOnScreen();
         this.primarystage.setTitle(String.format("Viewpoint Viewer: %s",name));
-        setModelXYBounds();
         this.primarystage.show();
     }
 
     /**
-     *
+     * This function needs to be called before creating a new model or opening an old one. The dimensions of the
+     * user's monitor are calculated in {@link vpvgui.ViewPointViewer} and transmitted to this class, whence they
+     * are provided to the {@link Model} object. The Model objects uses these dimensions only to set the width of
+     * the image that is obtained from the UCSC Genome browser (all other dimensions should be set automatically
+     * using the JavaFX code). If for any reason these dimensions are not set, the default values of
+     * {@link #screenHeight} and {@link #screenWidth} are used.
      * @param x X dimension of the user's screen
      * @param y Y dimension of the user's screen
      */
@@ -110,10 +111,6 @@ public class SwitchScreens {
         this.screenWidth=x;
     }
 
-    private void setModelXYBounds() {
-        model.setXdim(screenWidth);
-        model.setYdim(screenHeight);
-    }
 
 
 }
