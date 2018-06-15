0336af3c23541c1b0e8ab2ba53095e83bf376c9f
hansep <peter.hansen@charite.de>
2018-06-15 18:23:14 -0400

    Continued to implement alignability map (#145).

--- src/main/java/gopher/model/viewpoint/AlignabilityMap.java
+++ src/main/java/gopher/model/viewpoint/AlignabilityMap.java
@@ -1,9 +1,6 @@
 package gopher.model.viewpoint;
 
-import gopher.model.genome.Genome;
-import org.apache.commons.jexl2.UnifiedJEXL;
 import org.apache.log4j.Logger;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,117 +9,123 @@ import java.util.zip.GZIPInputStream;
 /**
  * Created by hansep on 6/12/18.
  *
+ * This class reads an array hash of pairs of arrays. The keys are chromosome names. The array pairs are combined
+ * in a simple private sub class (ArrayPair) of this class and consists of an Integer and a Double array. The first
+ * array has all positions at which the alignability score changes. The second array has the associated scores.
+ *
+ * This data structure can be efficiently queried for alignabilty scores at given positions. The search function
+ * performs a binary search on the array for position within the array pair object of the given chromosome. The
+ * determined index corresponds to the first position before the given postion at which the alignabilty score changes
+ * and can be used to fetch the alignabilty score from the second array.
+ *
  * Input bedGraph files are assumed to be sorted like this:
  *
  * sort -k1,1 -k2,2n hg19.100mer.alignabilityMap.bedgraph
  *
  * First column (chromosome) lexicographically, second column numerically start position.
  * The downloadable files are sorted like this.
- *
- *
  */
 public class AlignabilityMap {
     private static Logger logger = Logger.getLogger(AlignabilityMap.class.getName());
 
     /**
+     * Core function of this class.
      *
-     * Path including file name to gzipped bedGraph file.
-     *
+     * @param pos
+     * @return Score at pos
      */
-    private String alignabilityMapPathIncludingFileName = null;
-
-    /**
-     *
-     * Core structure of this class.
-     *
-     * Keys: chromosome names
-     *
-     * Values: pairs of arrays, one Integer array for the starting coordinates of intervals and a Double array for alignabilty scores.
-     */
-    private HashMap<String,ArrayPair> alignabilityMap = null; // key: chromosome name, value: starting position of an interval
-
-
-    AlignabilityMap(String alignabilityMapPathIncludingFileName) throws IOException {
-
-        this.alignabilityMapPathIncludingFileName = alignabilityMapPathIncludingFileName;
-
-
+    public Double getScoreAtPos(String chromosome, Integer pos) {
+        return 1.0;
     }
 
-    public boolean parseBedGraphFile() throws IOException {
+    /**
+     * Core structure of this class.
+     *
+     * Keys:
+     * chromosome names
+     *
+     * Values:
+     * pairs of arrays, one Integer array for the positions at which the alignability score changes
+     * and a Double array for alignabilty scores.
+     */
+    private HashMap<String,ArrayPair> alignabilityMap = null;
 
-        InputStream fileStream = new FileInputStream(this.alignabilityMapPathIncludingFileName);
+
+    /**
+     * Constructor
+     *
+     * @param alignabilityMapPathIncludingFileName Path including file name to gzipped bedGraph file.
+     * @throws IOException
+     */
+    AlignabilityMap(String alignabilityMapPathIncludingFileName) throws IOException {
+        this.parseBedGraphFile(alignabilityMapPathIncludingFileName);
+    }
+
+
+    /**
+     * Reads a bedGraph file to a data structure (alignabilityMap) described above.
+     *
+     * @throws IOException
+     */
+    public void parseBedGraphFile(String alignabilityMapPathIncludingFileName) throws IOException {
+
+        InputStream fileStream = new FileInputStream(alignabilityMapPathIncludingFileName);
         InputStream gzipStream = new GZIPInputStream(fileStream);
         Reader decoder = new InputStreamReader(gzipStream);
         BufferedReader br = new BufferedReader(decoder);
 
         try {
             String line;
-            String prevChr="NA";
-            Integer prevEnd = -1;
-            String prevLine = "NA";
-            while ((line=br.readLine())!=null) {
-                String A[]=line.split("\t");
+            String prevChr="chr0";
+            Integer prevEnd = 0;
+            while ((line = br.readLine()) != null) {
+
+                // extract information from line
+                String A[] = line.split("\t");
                 String chromosome = A[0];
-                Integer sta = Integer.parseInt(A[1]) + 1;             // start coordinates of the bedGraph format are 0-based
-                Integer end = Integer.parseInt(A[2]);                   // end coordinates of the bedGraph format are 1-based
+                Integer sta = Integer.parseInt(A[1]) + 1;                   // start coordinates of the bedGraph format are 0-based
+                Integer end = Integer.parseInt(A[2]);                       // end coordinates of the bedGraph format are 1-based
                 Double alignabilityScore= Double.parseDouble(A[3]);
 
-                if(!chromosome.equals("NA") && prevEnd != -1) { // first line of the file
+                int dist = sta - prevEnd;
+                if(!chromosome.equals(prevChr)) {
 
-                    ArrayPair arrayPair = new ArrayPair(); // create new pair of arrays
-                    alignabilityMap = new HashMap<String,ArrayPair>();
-                    alignabilityMap.put(chromosome, arrayPair);
-
-                    if(sta!=1) {
-                        alignabilityMap.get(chromosome).addCoordScorePair(1,-1.0); // no scores available for the first bases of the chromosome
+                    // this is the first line of the file for a new chromosome
+                    alignabilityMap = new HashMap<String, ArrayPair>();     // create new hash map for chromosome
+                    ArrayPair posVal = new ArrayPair();                     // create new pair of arrays
+                    alignabilityMap.put(chromosome, posVal);                // and put ararray pair to hash map
+                    if (1 != dist) {
+                        // there is a gap before the first region of the chromosome
+                        alignabilityMap.get(chromosome).addCoordScorePair(1, -1.0);
+                        alignabilityMap.get(chromosome).addCoordScorePair(sta, alignabilityScore);
+                    } else {
+                        alignabilityMap.get(chromosome).addCoordScorePair(sta, alignabilityScore);
                     }
-                    alignabilityMap.get(chromosome).addCoordScorePair(sta,alignabilityScore);
+
+                } else {
+
+                    // this is NOT the first line of the file for a new chromosome
+                    if (1 < dist) {
+                        // there is a gap before the current region
+                        alignabilityMap.get(chromosome).addCoordScorePair(1, -1.0);
+                        alignabilityMap.get(chromosome).addCoordScorePair(sta-1, alignabilityScore);
+                    } else {
+                        alignabilityMap.get(chromosome).addCoordScorePair(sta, alignabilityScore);
+                    }
                 }
-
-
-
-                    if(chromosome.equals(prevChr)) {
-                        Integer d = sta-prevEnd;
-                        if(d>1) { // there was a gap before the current region
-
-                            // add missing interval chr prevEnd + 1 with value -1
-
-
-                            logger.trace(chromosome + " " + d + " " + prevLine + " " + line);
-                        }
-
-                    }
-
-
-
-
-
-
                 prevChr = chromosome;
                 prevEnd = end;
-                prevLine = line;
-            }
+            } // end while
         } finally {
             br.close();
         }
-
-        return false;
     }
 
-    private class IntPair {
 
-        Integer sta;
-        Integer end;
-        IntPair(Integer sta, Integer end) {
-             this.sta = sta;
-             this.end = end;
-             if(end<=sta) {
-                 //throw Exception;
-             }
-        }
-    }
-
+    /**
+     * Consists of an Integer array containing to positions at which the alignability score changes in sorted order
+     * and a Double array with associtated scores.
+     */
     private class ArrayPair {
 
         private ArrayList<Integer> coordArray = null;
@@ -130,6 +133,7 @@ public class AlignabilityMap {
 
         public void addCoordScorePair(Integer sta, Double score) {
             if(coordArray == null) {
+                // these are the first values added to this object
                 coordArray = new ArrayList<Integer>();
                 scoreArray = new ArrayList<Double>();
             }
