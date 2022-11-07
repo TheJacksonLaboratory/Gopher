a7e530147f0ba506ea16a46f3421458a0ea7a8d6
Peter Robinson <peter.robinson@jax.org>
2022-11-07 16:22:46 -0500

    Adding runtime exception class

---
+++ src/main/java/gopher/exception/GopherRuntimException.java
@@ -0,0 +1,6 @@
+package gopher.exception;
+
+public class GopherRuntimException extends RuntimeException {
+    public GopherRuntimException() { super();}
+    public GopherRuntimException(String m) { super(m);}
+}
