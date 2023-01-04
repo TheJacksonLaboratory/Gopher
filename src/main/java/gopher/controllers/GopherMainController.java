d4acdf28e4ab0511b8e4180d4b7bbd49581579c2
Peter Robinson <peter.robinson@jax.org>
2023-01-04 05:16:06 -0500

    fixing delete dialog (3/3)

--- src/main/java/gopher/controllers/GopherMainController.java
+++ src/main/java/gopher/controllers/GopherMainController.java
@@ -32,11 +32,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.layout.VBox;
@@ -1319,21 +1316,7 @@ public class GopherMainController implements Initializable {
     @FXML
     public void deleteProjectFiles(ActionEvent e) {
         List<ProjectFile> pfiles = gopher.Platform.getProjectFiles();
-        try {
-            URL resource = getClass().getResource("/fxml/delete.fxml");
-            LOGGER.error("Deleting project files. FXML resource is {}", resource);
-            FXMLLoader fxmlLoader = new FXMLLoader(resource);
-            Parent parent = fxmlLoader.load();
-            deleteController.updateTable(pfiles);
-
-            Scene scene = new Scene(parent, 300, 200);
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setScene(scene);
-            stage.showAndWait();
-        } catch (IOException ex) {
-            PopupFactory.displayException(ex);
-        }
+        DeleteProjectFilesFactory.dispay(pfiles);
         e.consume();
     }
 
