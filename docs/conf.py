e6670f80612daaaf25de1a6fe50d1749d256e7fb
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-06-14 09:50:27 -0400

    Update the CSS location.

--- docs/conf.py
+++ docs/conf.py
@@ -94,6 +94,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -166,7 +167,3 @@ texinfo_documents = [
      author, 'GOPHER', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-def setup(app):
-#   app.add_javascript("custom.js")
-   app.add_stylesheet("custom.css")
